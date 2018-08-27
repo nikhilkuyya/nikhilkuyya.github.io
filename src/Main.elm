ac77727cde0e4dd5055c30d02aa1e2461f6e62c9
nikhilkuyya <nikhilkuyya@gmail.com>
2018-08-27 14:32:09 +0530

    elm setup

---
+++ src/Main.elm
@@ -0,0 +1,13 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+-- MODEL
+
+-- VIEW
+
+-- UPDATE
+
+-- MAIN
+
+main = div [] [text "Hello world, this is Elm"]
