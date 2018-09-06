ed3774e963a512e055cb1798c136b9dc973102b4
nikhilkuyya <nikhilkuyya@gmail.com>
2018-09-06 18:54:26 +0530

    json format of resume

---
+++ assests/resume-content.js
@@ -0,0 +1,46 @@
+loadContent([
+  {
+    heading: "Career Summary",
+    class: "career-summary",
+    data:
+      "I work on Front End Technologies, also had experience in the Back-End development. I am inclined to work with different technologies with good practices and worked on diverse projects to gain an understanding of the software development process."
+  },
+  {
+    heading: "Work Experience",
+    class: "work-experience",
+    data: {
+      heading: "Nagarro",
+      caption: "Gurgaon,India",
+      data: [
+        {
+          heading: "Associate",
+          caption: "Oct,2017 - Present",
+          data: [
+            {
+              heading: "Responsible for shipping web pages for website.",
+              data: [
+                "Understanding the requirements clearly and building components for pages",
+                "Building modules for pages, from scratch, if need or using appropriate libraries for development.",
+                "Worked on the distinct sections for websites with documentation for the work."
+              ]
+            }
+          ]
+        },
+        {
+          heading: "Junior Associate",
+          caption: "Oct 2016 - Sep 2017",
+          data: [
+            {
+              heading: "Explored diverse projects relating DOTNET Framework.",
+              data: [
+                "Implemented features like managing configuration files and databases to Windows Installer.",
+                "United features to Excel Plugin to fetch workbook data.",
+                "Worked on building and bug fixes for services in automation project."
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+]);
